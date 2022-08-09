1ba5719dcf00e94426f8b82edaa389f1f7443903
Your Name <you@example.com>
2022-08-09 17:23:13 +0530

    Added: First commit

---
+++ src/test/java/com/LoginPage/firstservlet/FirstServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LoginPage.firstservlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
